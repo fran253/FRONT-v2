4b8a2e49d0ee8dea550c98acaae538c09dbe91f2
fran253 <123721486+fran253@users.noreply.github.com>
2025-02-04 17:35:44 +0100

    Add files via upload

---
+++ src/components/Header.vue
@@ -0,0 +1,59 @@
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<template>
+  <header class="header">
+    <div class="logo">
+      <img src="../images/logo.png" alt="logo" class="logo-image">
+    </div>
+    <nav class="menu">
+      <router-link to="/peliculas">PELICULAS</router-link>
+      <router-link to="#">MENU</router-link>
+      <router-link to="#ENCUENTRANOS">
+        <img src="../images/chincheta.png" alt="Ubicación">
+      </router-link>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+.header {
+    margin-top: 4%;
+    margin-bottom: 1.5%;
+    padding: 1px 0;
+
+    .logo {
+        .logo-image {
+            width: 250px; 
+            height: auto;
+        }
+    }
+
+    .menu {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        z-index: 1000; 
+        background-color: #FFC300;
+        display: flex;
+        justify-content: center; 
+        padding: 19px 0;
+
+        a {
+            color: black;
+            text-decoration: none;
+            font-size: 1rem; 
+            font-family: 'Jura', sans-serif;
+            margin: 0 50px; 
+
+            &:hover {
+                color: #333333;
+            }
+        }
+    }
+}
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,63 @@
+<script setup>
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+</script>
+
+<template>
+  <Header />
+
+  <Banner />
+
+  <InfoCine />
+
+  <Carrusel />
+
+  <Mapa />
+
+  <Footer />
+</template>
+
+<style scoped>
+.banner {
+  width: 100%;
+  position: relative;
+}
+
+.banner-image {
+  width: 100%;
+  height: auto;
+}
+
+.info-section {
+  padding-top: 5%;
+}
+
+.info-content {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 260px;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+
+.info-logo-text {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  gap: 40px;
+  width: 30%;
+}
+
+.info-logo {
+  width: 100%;
+  max-width: 250px;
+}
+
+.info-description {
+  font-size: 1.2rem;
+  line-height: 1.6;
+  width: 400px;
+}
+</style>
