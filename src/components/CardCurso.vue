b6960de3c6961950376cb50d50201a9451aa26d4
Diegorodriguezaznar <a25625@svalero.com>
2025-02-25 19:07:03 +0100

    Usuario CON TO CRUD-O lo pillas?

--- src/components/CardCurso.vue
+++ src/components/CardCurso.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref, computed } from 'vue';
 import { useRouter } from 'vue-router';
-
+import type { CursoDTO } from "@/stores/dtos/CursoDTO";
 const router = useRouter();
 
 const curso = defineProps({
