362eae6a19534c51311c51d72e927a7c789eafc5
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2025-02-27 17:47:52 +0100

    Pequeños cambios de diseño

--- src/components/CardAsignatura.vue
+++ src/components/CardAsignatura.vue
@@ -22,13 +22,15 @@
 
 <template>
   <v-card class="mx-auto" max-width="400" @click="seleccionarAsignatura">
-    <v-img class="align-end text-white" height="200" :src="asignatura.imagen" cover>
+    <v-img class="align-end text-white" height="150" :src="asignatura.imagen" cover>
       <v-card-title>{{ asignatura.nombre }}</v-card-title>
     </v-img>
 
-    <v-card-subtitle class="pt-4">
+    <v-card-subtitle class="pt-4 text-orange-darken-2 text-h6">
       {{ asignatura.nombre }}
     </v-card-subtitle>
+
+
 
     <v-card-actions>
 
